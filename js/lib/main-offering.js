fca3687f64c67ecd8334c234f5273033ffcbd723
Battula Sai Vinesh <vineshbattula@gmail.com>
2017-12-16 16:25:46 +0530

    Added Offering,workflow page templates

---
+++ js/lib/main-offering.js
@@ -0,0 +1,21 @@
+
+
+var startTween = new TimelineMax()
+				.to("#particles-js",20,{opacity:1});
+
+
+
+var startTween2 = new TimelineMax()
+				.to("#title #heading",1,{y:'-40px'})
+				.to("#title #heading span",1,{opacity:1},0);
+				
+
+
+
+animateStartArrow = function(e, t, i) { var a = $(".intro-start-arrow"),
+                    o = $(".intro-start-arrow path")[0],
+                    s = o.getTotalLength(),
+                    r = { progress: 1 };
+                o.style.strokeDasharray = s + " " + s, o.style.strokeDashoffset = s, a.css("visibility", "visible"), e.fromTo(r, t, { progress: 1 }, { progress: 0, onUpdateParams: [o, r], onUpdate: function(e, t) { e.style.strokeDashoffset = t.progress * s }, ease: Quad.easeOut }, i) },
+
+animateStartArrow(startTween2, .5, 1.5);
